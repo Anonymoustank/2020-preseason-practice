5161f29a19dc80adc2325142f24abddecad1aa61
Anonymoustank <pkadekodi@gmail.com>
2019-11-10 14:16:08 -0700

    Did some more Euler problems

--- src/main/kotlin/Euler4.kt
+++ src/main/kotlin/Euler4.kt
@@ -1,34 +1,7 @@
-object main {
+object main2 {
     @JvmStatic
     fun main(args: Array<String>) {
         println("Hello World")
-    var x = 0
-    var y = 0
-    var sum_even = 0
-    for (i in 1..10){
-    	if (x == 0){
-    		println(x)
-    		x += 1
-    		println(x)
-    	}
-    	x += y
-    	y = (x - y)
-    	println(x)
-    	if (x % 2 == 0){
-    		sum_even += x
-    	}
-
-    }
-    println("The sum of the evens is...")
-    println(sum_even)
-
-    var sum_natural = 0
-    for (i in 1..999){
-    	if ((i % 3 == 0) || (i % 5 == 0)){
-    		sum_natural += i
-    	}
-    }
-    println(sum_natural)
     var highest_number = 0
     for (i in 100..999){
     	for (x in 100..999){
