45bb97065df27ecf7fc0935921ca78d0a9572200
Anonymoustank <pkadekodi@gmail.com>
2019-11-13 19:42:01 -0700

    Tweaked Euler 7 solution

--- src/main/kotlin/Euler7.kt
+++ src/main/kotlin/Euler7.kt
@@ -2,10 +2,10 @@ package practice;
 
 object main4 {
     fun main() {
-        var prime_found = 2
+        var prime_found = 1
         var number_check = 3
         var isPrime = true
-        while (prime_found != 6){
+        while (prime_found != 50){
             for (i in 2..number_check-1){
                 if (number_check % i == 0){
                     isPrime = false
