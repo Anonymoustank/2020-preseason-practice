9fcbf8d1e85cd76acb2ba739204dcd76bc179723
Anonymoustank <pkadekodi@gmail.com>
2019-11-19 23:15:31 -0700

    Euler10.kt

--- src/main/kotlin/Euler10.kt
+++ src/main/kotlin/Euler10.kt
@@ -4,7 +4,7 @@ object main6 {
     fun main() {
         var prime_total = 5
         var number_check = 3
-        var number_limit = 100000
+        var number_limit = 100
         var isPrime = true
         for (i in 4..number_limit){
             for (x in 2..i-1){
